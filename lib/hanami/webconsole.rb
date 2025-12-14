9f162425cb3dbc603876d1b7accf14c4c928450c
Tim Riley <tim@riley.id.au>
2025-12-14 21:43:48 +1100

    Allow binding_of_caller to load under Ruby 4.0
    This requires a bit of a hack due to binding_of_caller not yet having a new release to introduce Ruby 4.0 compatibility.

--- lib/hanami/webconsole.rb
+++ lib/hanami/webconsole.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
+if RUBY_ENGINE == "ruby"
+  # Skip binding_of_caller's version check and force it to load on Ruby 4.0.
+  #
+  # Remove this shim once https://github.com/banister/binding_of_caller/pull/90 is merged and a new
+  # release has been made.
+  require "binding_of_caller/version"
+  require "binding_of_caller/mri"
+
+  # Prevent a direct require to "binding_of_caller", which is no longer needed by this point, and
+  # which prints a misleading warning about it not supporting Ruby 4.0, which we've fixed ourselves
+  # via the above.
+  $LOADED_FEATURES << "binding_of_caller.rb"
+else
+  require "binding_of_caller"
+end
+
 require "better_errors"
-require "binding_of_caller"
 
 module Hanami
   # Hanami web console for development
